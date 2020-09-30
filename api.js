32780e18ed47921779fbed085ae40d32d92562b5
fleece30 <abhiyad25@gmail.com>
2020-09-30 21:11:10 +0530

    hacktoberfest issues

--- api.js
+++ api.js
@@ -1,17 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('./scraper');
+const data = require("./scraper");
 
-router.get('/getstate/:name', (req, res) => {
-    const reqState = req.params.name;
-    const result = data.states.find(result => result.trimmedState === reqState);
-    res.json(result);
+router.get("/getall", (req, res) => {
+  console.log(data.states);
+  console.log(data.country);
+  res.json(data.country[0]);
 });
-
-router.get('/getall', (req, res) => {
-    console.log(data.states);
-    console.log(data.country);
-    res.json(data.country[0]);
-})
 
 module.exports = router;
--- scraper.js
+++ scraper.js
@@ -1,45 +1,31 @@
 const siteURL = "https://www.mohfw.gov.in/";
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 const states = [];
 const country = [];
 
 const fetchData = async () => {
-    const result = await axios.get(siteURL);
-    return cheerio.load(result.data);
-}
+  const result = await axios.get(siteURL);
+  return cheerio.load(result.data);
+};
 
 const printData = async () => {
-    const $ = await fetchData();
-    $('.site-stats-count > ul').each((index, element) => {
-        const ele = $(element).text().trim().split('\n');
-        const data = {
-            'active': ele[0].trim(),
-            'cured': ele[5].trim(),
-            'deaths': ele[12].trim(),
-            'migrated': ele[17].trim()
-        }
-        country.push(data);
-    });
-} 
+  const $ = await fetchData();
+  $(".site-stats-count > ul").each((index, element) => {
+    const ele = $(element).text().trim().split("\n");
+    const data = {
+      active: ele[0].trim(),
+      cured: ele[5].trim(),
+      deaths: ele[12].trim(),
+      migrated: ele[17].trim(),
+    };
+    country.push(data);
+  });
+};
 
-const printStateData = async () => {
-    const $ = await fetchData();
-    $('.data-table > table > tbody > tr').each((index, element) => {
-        if(index>31) return false;
-        const ele = $(element).text().trim().split('\n');
-        const data = {
-            'trimmedState': ele[1].toLowerCase().trim().replace(/ +/g, ""),
-            'stateName': ele[1].trim(), 
-            'total': parseInt(ele[2].trim()),
-            'recovered': parseInt(ele[3].trim()),
-            'deaths': parseInt(ele[4].trim()),
-        }
-        states.push(data);
-    })
-} 
+//add function here
 
 printStateData();
 printData();
 
-module.exports = {states, country};
+module.exports = { states, country };
